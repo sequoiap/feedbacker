975367220ec451bd165e1a6b964752a20838681f
Sequoia Ploeg <sequoiap4@gmail.com>
2024-09-22 22:25:42 -0600

    Convert to typehinted

--- src/feedbacker/assignments/models.py
+++ src/feedbacker/assignments/models.py
@@ -1,20 +1,21 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from feedbacker.database import Base
+from feedbacker.auth.models import User  # noqa # pylint: disable=unused-import
 
 
 class Assignment(Base):
     __tablename__ = "assignments"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(index=True)
+    description: Mapped[str] = mapped_column(index=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
 
-    user = relationship("User", back_populates="assignments")
-    feedbacks = relationship("Feedback", back_populates="assignment")
+    user = relationship("User")#, back_populates="assignments")
+    # feedbacks = relationship("Feedback", back_populates="assignment")
