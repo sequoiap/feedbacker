5d65346b1526545eb4cbcafce21c88a4ae39b034
Sequoia Ploeg <sequoiap4@gmail.com>
2025-03-16 22:01:18 -0600

    Specify language

--- src/feedbacker-ui/quasar.config.js
+++ src/feedbacker-ui/quasar.config.js
@@ -82,7 +82,7 @@ export default defineConfig((/* ctx */) => {
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
-      // lang: 'en-US', // Quasar language pack
+      lang: 'en-US', // Quasar language pack
 
       // For special cases outside of where the auto-import strategy can have an impact
       // (like functional components as one of the examples),
