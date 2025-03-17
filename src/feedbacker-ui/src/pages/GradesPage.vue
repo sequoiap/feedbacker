aef282b1ca2c89aca13640efa08869306cd90d76
Sequoia Ploeg <sequoiap4@gmail.com>
2025-03-16 22:00:45 -0600

    Add expandable rows to grades for future attempts viewer

--- src/feedbacker-ui/src/pages/GradesPage.vue
+++ src/feedbacker-ui/src/pages/GradesPage.vue
@@ -3,20 +3,61 @@
     <h1>Grades</h1>
 
     <div class="q-pa-md">
+
       <q-table
-        title="Grades"
         :rows="grades"
         :columns="columns"
-        row-key="name"
-      />
+        row-key="assignment"
+        v-model:expanded="expanded"
+      >
+        <template v-slot:header="props">
+          <q-tr :props="props">
+            <q-th auto-width />
+
+            <q-th
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+            >
+              {{ col.label }}
+            </q-th>
+          </q-tr>
+        </template>
+        
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td auto-width>
+              <!-- <q-toggle v-model="props.expand" checked-icon="add" unchecked-icon="remove" /> -->
+              <q-btn size="sm" color="accent" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'remove' : 'add'" />
+            </q-td>
+
+            <q-td
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+            >
+              {{ col.value }}
+            </q-td>
+          </q-tr>
+          
+          <q-tr v-show="props.expand" :props="props">
+            <q-td colspan="100%">
+              <div class="text-left">This is expand slot for row above: {{ props.row.assignment }}.</div>
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+
     </div>
   </q-page>
 </template>
 
 <script setup>
-import { inject, onMounted } from 'vue'
+import { inject, onMounted, ref } from 'vue'
 
 const updateDrawer = inject('updateDrawer');
+
+const expanded = ref([]);
 
 onMounted(() => {
   updateDrawer([
@@ -37,7 +78,7 @@ onMounted(() => {
 
 const columns = [
   {
-    name: 'name',
+    name: 'title',
     required: true,
     label: 'Assignment',
     align: 'left',
